8c0554e2adfb1e59e483895215d12bc7dfe93402
CHENG Xin <mrchengxin@gmail.com>
2022-03-09 12:14:44 +0800

    build the music-list popup

--- 01_frontend/src/app/shared-components/music-player/music-player.component.ts
+++ 01_frontend/src/app/shared-components/music-player/music-player.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-music-player',
   templateUrl: './music-player.component.html',
-  styleUrls: ['./music-player.component.css']
+  styleUrls: ['./music-player.component.css'],
 })
 export class MusicPlayerComponent implements OnInit {
+  showMusicList: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  mouseOverMusicList(): void {
+    this.showMusicList = true;
   }
 
+  mouseOutMusicList(): void {
+    this.showMusicList = false;
+  }
 }
